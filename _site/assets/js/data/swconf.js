0a43886bb57ccb9390245beaa5af39ee3556c466
Abhishek Pandey <abhishekgpandey7@gmail.com>
2025-11-13 13:22:45 +0000

    Publish site to gh-pages (manual)

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1763040058',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/about/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
